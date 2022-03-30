358d24059269559c090f64ad4781b76463f352d1
Aria Koul <ariakoul@gmail.com>
2022-03-29 19:48:23 -0700

    Made the three utility functions for the game

---
+++ utility_functions.py
@@ -0,0 +1,24 @@
+# Imports here
+
+def generate_word():
+    """
+    Generates a random five letter word in the English language.
+    This five letter word is the solution to the game. 
+    """
+    return
+
+def word_guess():
+    """
+    Gets user's guess for what the solution could be.
+    """
+    return
+
+def score_guess(guess, solution):
+    """
+    Takes the user's guess and scores it based on what the solution is. Below is a key for how
+    you can interpret the score:
+        X - character is not in the word
+        Y - character is in the word but in the wrong spot
+        Z - character is in the word and in the right spot
+    """
+    return
