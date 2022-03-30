858ae85d868eb3708afcacb4feda67c667176059
Aria Koul <ariakoul@gmail.com>
2022-03-30 15:13:43 -0700

    Created the logic for generate_word()

--- utility_functions.py
+++ utility_functions.py
@@ -1,11 +1,26 @@
-# Imports here
+from english_words import english_words_set
+import random
 
 def generate_word():
     """
     Generates a random five letter word in the English language.
     This five letter word is the solution to the game. 
     """
-    return
+    # Make a list of all possible five letter words in the English language
+    five_letter_words = []
+    for a_word in english_words_set:
+        if len(a_word) == 5:
+            five_letter_words.append(a_word)
+    
+    # Check if any word is a proper noun and remove it from the list of possible five letter words
+    for word in five_letter_words:
+        if word[0].isupper():
+            five_letter_words.remove(word)
+    
+    # Pick a random five letter word to be the solution 
+    solution = random.choice(five_letter_words)
+    
+    return solution
 
 def word_guess():
     """
@@ -22,3 +37,5 @@ def score_guess(guess, solution):
         Z - character is in the word and in the right spot
     """
     return
+
+generate_word()
