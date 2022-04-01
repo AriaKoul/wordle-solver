440453d982563a357079de20b865b3e94fdb8895
Aria Koul <ariakoul@gmail.com>
2022-03-31 20:05:16 -0700

    Created logic for more functions in utility_functions.py
    Created the logic for score_guess() and added the functions for the solver part of the file

--- utility_functions.py
+++ utility_functions.py
@@ -1,25 +1,32 @@
 from english_words import english_words_set
 import random
 
+def generate_list():
+    """
+    Plurals are not included in this and we can add the "s" to any four letter word.
+    This function creates a list full of all five letter words in the English language.
+    """
+    five_letter_words = []
+    for word in english_words_set:
+        # if len(word) == 4 and word[0].islower():
+        #     five_letter_words.append(f"{word}s")
+
+        if len(word) == 5 and word[0].islower():
+            five_letter_words.append(word)
+    
+    return five_letter_words
+
 def generate_word():
     """
     Generates a random five letter word in the English language.
     This five letter word is the solution to the game. 
     """
-    # Make a list of all possible five letter words in the English language
-    global five_letter_words
-    five_letter_words = []
-    for word in english_words_set:
-        if len(word) == 5 and word[0].islower():
-            five_letter_words.append(word)
-    
+
     # Pick a random five letter word to be the solution 
-    solution = random.choice(five_letter_words)
-    print(solution)
-    
+    solution = random.choice(generate_list())
     return solution
 
-def word_guess(five_letter_words):
+def word_guess():
     """
     Gets user's guess for what the solution could be.
     Asks for another input if the user's guess doesn't satisfy the following two conditions:
@@ -27,25 +34,45 @@ def word_guess(five_letter_words):
         2) Word is in the list five_letter_words (is real and isn't a proper noun)
     """
     # Gets user input
-    guess = input('Enter your five-letter word guess here: ')
-
+    guess = input('Enter your five-letter word guess here: ').lower()
     # Checks that user's input satisfies all requirements and if not, asks for another guess
-    while len(guess) != 5 or guess not in five_letter_words:
-        guess = guess.lower()
-        if len(guess) < 5 or len(guess) > 5 or guess not in five_letter_words:
-            guess = input('Make sure your guess is five letters long, not a proper noun, and is real. Guess another word: ')
+    while len(guess) != 5 or guess not in generate_list():
+        guess = input('Make sure your guess is five letters long, not a proper noun, and is real. Guess another word: ')
 
     return guess
 
-def score_guess(guess, solution):
+def score_guess(solution, guess):
     """
     Takes the user's guess and scores it based on what the solution is. Below is a key for how
     you can interpret the score:
-        X - character is not in the word
+        R - character is not in the word
         Y - character is in the word but in the wrong spot
-        Z - character is in the word and in the right spot
+        G - character is in the word and in the right spot
     """
-    return
+    result = ''
+    for i in range(len(solution)):
+        if solution[i] == guess[i]:
+            result += 'G'
+        elif guess[i] in solution:
+            result += 'Y'
+        else:
+            result += 'R'
 
-generate_word()
-word_guess(five_letter_words)
+    return result
+
+
+def generate_possible_solutions(guess_history):
+    # guess_history is a dictionary with a structure {"guess": "result"}
+    possible_solutions = generate_list()
+    # Iterate over items in guess history to remove invalid words from possible solutions
+    return possible_solutions
+
+def generate_best_guesses(word_list):
+    """
+    This function takes a list of words and returns all the words with the maximum amount of 
+    unique letters.
+    """
+    best_guesses = []
+    # for word in 
+    return best_guesses 
+
